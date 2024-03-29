7b570d985d1e2461e75def59c484266430ce0938
Mann Patel <mannpatel@Manns-MacBook-Pro.local>
2024-03-29 22:42:17 +0530

    My First Commit

---
+++ backend/config.js
@@ -0,0 +1,6 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://22it091:abc123456@book-store-mern.ujuq6er.mongodb.net/books-collection?retryWrites=true&w=majority&appName=book-store-mern'
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
